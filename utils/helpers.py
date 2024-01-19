55bdb02cb447e88d417e1abba7db49db90de99d8
Jhonathan Segura <jhonathanssegura@gmail.com>
2024-01-19 10:43:09 -0500

    Adding folder utils with helpers to do

---
+++ utils/helpers.py
@@ -0,0 +1,5 @@
+def process_data(csv_data, api_data):
+    # Add your data processing logic here
+    # For example, combining and transforming data from CSV and API
+    processed_data = {'csv_data': csv_data, 'api_data': api_data}
+    return processed_data
